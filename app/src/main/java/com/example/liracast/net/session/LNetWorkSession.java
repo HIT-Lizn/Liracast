cc0e03eac409dabc53ec7bf5b6fead9729afd10a
lizn <lixin8@oppo.com>
2022-01-19 17:56:19 +0800

    add network session

--- app/src/main/java/com/example/liracast/net/session/LNetWorkSession.java
+++
@@ -1,4 +0,0 @@
-package com.example.liracast.net.session;
-
-public class LNetWorkSession {
-}
---
+++ app/src/main/java/com/example/liracast/net/session/LiNetWorkSession.java
@@ -0,0 +1,48 @@
+package com.example.liracast.net.session;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+import com.example.liracast.net.tcp.TCPSender;
+import com.example.liracast.net.tcp.TCPServier;
+import com.example.liracast.net.ucp.UDPRecevier;
+import com.example.liracast.net.ucp.UDPSender;
+
+public class LiNetWorkSession {
+    private final String TAG = "LiNetWorkSession";
+    private Handler mNotify;
+    private Handler mHandler;
+    private HandlerThread mHandlerThread;
+    private TCPSender mTcpSender;
+    private TCPServier mTcpServier;
+    private UDPSender mUdpSender;
+    private UDPRecevier mUdpRecevier;
+
+    public LiNetWorkSession(Handler notify) {
+        mNotify = notify;
+        mHandlerThread = new HandlerThread("LiNetWorkSession");
+        mHandlerThread.start();
+        Looper looper = mHandlerThread.getLooper();
+        if (looper != null) {
+            mHandler = new NetWorkSessionHandler(looper);
+        } else {
+            Log.e(TAG, "looper == null");
+        }
+    }
+
+
+    public class NetWorkSessionHandler extends Handler {
+        public NetWorkSessionHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            //To be continue
+        }
+    }
+}
